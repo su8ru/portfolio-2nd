d33c641ce076a8c10769ecd0f192acb523205947
su8ru <s@su8ru.dev>
2020-04-03 00:27:02 +0900

    feat: add skills

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -45,7 +45,128 @@
         </tbody>
       </table>
     </stack-box>
-    <stack-box color="#dfffc1" title="skill" icon="terminal"></stack-box>
+    <stack-box color="#dfffc1" title="skill" icon="terminal">
+      <table>
+        <tbody>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="html5" />
+                HTML5
+              </h3>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="css3" />
+                CSS3
+              </h3>
+            </th>
+            <td>
+              <ul>
+                <li>
+                  <simple-icons name="sass" />
+                  Sass
+                </li>
+                <li>
+                  <simple-icons name="bootstrap" />
+                  Bootstrap
+                </li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="typescript" />
+                TypeScript
+              </h3>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="php" />
+                PHP
+              </h3>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="vue-dot-js" />
+                Vue.js
+              </h3>
+            </th>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="cplusplus" />
+                C++
+              </h3>
+            </th>
+            <td>
+              <ul>
+                <li>
+                  <simple-icons name="visualstudio" />
+                  Visual Studio
+                </li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="git" />
+                Git
+              </h3>
+            </th>
+            <td>
+              <ul>
+                <li>
+                  <simple-icons name="github" />
+                  GitHub
+                </li>
+                <li>
+                  <simple-icons name="githubactions" />
+                  GitHub Actions
+                </li>
+                <li>
+                  <simple-icons name="gitkraken" />
+                  GitKraken
+                </li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="adobeillustrator" />
+                Illustrator
+              </h3>
+            </th>
+            <td>
+              <ul>
+                <li>
+                  <simple-icons name="svg" />
+                  SVG
+                </li>
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th>
+              <h3>
+                <simple-icons name="jetbrains" />
+                JetBrains
+              </h3>
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </stack-box>
     <stack-box color="#ffe1c1" title="work" icon="folder"></stack-box>
     <stack-box color="#ffc1df" title="contact" icon="envelope"></stack-box>
   </div>
